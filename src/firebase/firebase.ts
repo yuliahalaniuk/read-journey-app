b21b291f554e0e31586908974284af8fe02ffbf3
yuliahalaniuk <122388855+yuliahalaniuk@users.noreply.github.com>
2024-12-03 02:11:05 +0200

    Update firebase.ts

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -2,10 +2,21 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 import { getFirestore } from "firebase/firestore";
-import { Service } from "../service/ConfigsService";
+// import { Service } from "../service/ConfigsService";
 //ConfigsService
-const firebaseConfig = Service.getForFirebase();
+// const firebaseConfig = Service.getForFirebase();
 
+ const firebaseConfig = {
+   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+   appId: process.env.REACT_APP_FIREBASE_APP_ID,
+   measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+   databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+ };
+      
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
